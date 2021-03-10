ba81d2f47346bbd977687671f3b399a387a505a4
sueda akın <sueda_akin@hotmail.com>
2021-03-10 23:22:24 +0300

    aa

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+ 
+  
+  products: Product[] = [];
+  apiUrl="https://localhost:44345/api/products/getall";
+  constructor(private httpClient:HttpClient) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts(){
+   this.httpClient
+   .get<ProductResponseModel>(this.apiUrl)
+   .subscribe((response) => {
+     this.products=response.data
+   });
+  }
+
+}
